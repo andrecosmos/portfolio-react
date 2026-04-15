23672150a3c891f92d4ab2c3c71388fa108559e9
andrecosmos <andrecosmos.1992@gmail.com>
2026-04-14 23:14:39 -0300

    Acerto de imagens 2

--- src/Service/Dados.jsx
+++ src/Service/Dados.jsx
@@ -1,5 +1,24 @@
 
 import cvenda from "../assets/cvenda.png";
+import react1 from "../assets/react1-1.png";
+import react_portfolio from "../assets/react_portfolio.png";
+import api_java from "../assets/api_java1.png";
+import python1 from "../assets/python1.png";
+import conv_moeda_java from "../assets/conv_moeda_java.png";
+import livraria_java from "../assets/livraria_java.png";
+import banco_python from "../assets/banco_python.png";
+import item1-1 from "../assets/item1-1.png";
+import item1-2 from "../assets/item1-2.png";
+import item1-3 from "../assets/item1-3.png";
+import item1-4 from "../assets/item1-4.png";
+import item1-5 from "../assets/item1-5.png";
+import item1-6 from "../assets/item1-6.png";  
+import item1-7 from "../assets/item1-7.png";
+import item1-8 from "../assets/item1-8.png";
+import item1-9 from "../assets/item1-9.png";
+import item1-10 from "../assets/item1-10.png";  
+
+
 
 export const projetos = [
   {
@@ -9,42 +28,42 @@ export const projetos = [
   },
   {
     nome: "E-commerce React",
-    imagem: "src/assets/react1-1.png",
+    imagem: react1-1,
     link: "https://eccomerce-react-zeta.vercel.app/",
   },
   {
     nome: "Portfólio em React",
-    imagem: "src/assets/react_portfolio.png",
+    imagem: react_portfolio,
     link: "#",
   },
   {
     nome: "ApiREST Java",
-    imagem: "src/assets/api_java1.png",
+    imagem: api_java,
     link: "https://github.com/andrecosmos/apirestforum",
   },
   {
     nome: "Calcula preço em Python",
-    imagem: "src/assets/python1.png",
+    imagem: python1,
     link: "https://github.com/andrecosmos/CALCULA-PRECO",
   },
   {
     nome: "Conversor de Moedas",
-    imagem: "src/assets/conv_moeda_java.png",
+    imagem: conv_moeda_java,
     link: "https://github.com/andrecosmos/DesafioAluraOneConversor",
   },
   {
     nome: "Livraria em Java",
-    imagem: "src/assets/livraria_java.png",
+    imagem: livraria_java,
     link: "https://github.com/andrecosmos/onebookstore/tree/main/Onebookstore",
   },
   {
     nome: "Banco em Python",
-    imagem: "src/assets/banco_python2.png",
+    imagem: banco_python,
     link: "https://github.com/andrecosmos/BancoVersao3",
   },
   {
     nome: "Calculadora de Vendas",
-    imagem: "src/assets/cvenda.png",
+    imagem: cvenda,
     link: "https://projeto-calcula-venda-jjbt.vercel.app/",
   }
 
@@ -53,52 +72,52 @@ export const projetos = [
 export const habilidades = [
   {
     nome: "Javascript",
-     imagem: "src/assets/item1-1.png",
+     imagem: item1-1,
     link: "https://eccomerce-react-zeta.vercel.app/",
   },
   {
     nome: "JWT",
-     imagem: "src/assets/item1-2.png",
+     imagem: item1-2,
     link: "https://eccomerce-react-zeta.vercel.app/",
   },
   {
     nome: "React Native",
-     imagem: "src/assets/item1-3.png",
+     imagem: item1-3,
     link: "https://eccomerce-react-zeta.vercel.app/",
   },
   {
     nome: "Python",
-     imagem: "src/assets/item1-4.png",
+     imagem: item1-4,
     link: "https://eccomerce-react-zeta.vercel.app/",
   },
   {
     nome: "PostgreSQL",
-     imagem: "src/assets/item1-5.png",
+     imagem: item1-5,
     link: "https://eccomerce-react-zeta.vercel.app/"
   },
   {
     nome: "Node.js",
-     imagem: "src/assets/item1-6.png",
+     imagem: item1-6,
     link: "https://eccomerce-react-zeta.vercel.app/"
   },
   {
     nome: "MongoDB",
-     imagem: "src/assets/item1-7.png",
+     imagem: item1-7,
     link: "https://eccomerce-react-zeta.vercel.app/"
   },
   {
     nome: "Tailwind CSS",
-     imagem: "src/assets/item1-8.png",
+     imagem: item1-8,
     link: "https://eccomerce-react-zeta.vercel.app/"
   },
   {
     nome: "Prisma",
-     imagem: "src/assets/item1-9.png",
+     imagem: item1-9,
     link: "https://eccomerce-react-zeta.vercel.app/"
   },
   {
     nome: "Typescript",
-     imagem: "src/assets/item1-10.png",
+     imagem: item1-10,
     link: "https://eccomerce-react-zeta.vercel.app/"
   }
 
